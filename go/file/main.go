947aeaeb9cbd6c6098238c961ea908b00b9c134a
duke.yang <duke.yang@ucloud.cn>
2019-11-04 14:33:19 +0800

    add readLine

--- go/file/main.go
+++ go/file/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -28,7 +30,7 @@ func go_1_compatible() {
 		}
 	}()
 
-	buf := make([]byte, 1024)
+	buf := make([]byte, 10)
 	for {
 		// read a chunk
 		num, err := fi.Read(buf)
@@ -56,7 +58,7 @@ func use_bufio() {
 		}
 	}()
 	// make a read buffer
-	r := bufio.NewReader(fi)
+	r := bufio.NewReaderSize(fi, 32)
 
 	fo, err := os.Create("output.txt")
 	if err != nil {
@@ -70,10 +72,11 @@ func use_bufio() {
 	// make a write buffer
 	w := bufio.NewWriter(fo)
 
-	buf := make([]byte, 1024)
+	buf := make([]byte, 39)
 	for {
 		// read a chunk
 		num, err := r.Read(buf)
+		fmt.Println(num)
 		if err != nil && err != io.EOF {
 			panic(err)
 		}
@@ -101,8 +104,94 @@ func use_ioutil() {
 	}
 }
 
+func readLine() {
+	fi, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := fi.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	// make a read buffer
+	r := bufio.NewReaderSize(fi, 10)
+	for {
+		var buffer bytes.Buffer
+		var (
+			line     []byte
+			isPrefix bool
+		)
+		for {
+			line, isPrefix, err = r.ReadLine()
+			buffer.Write(line)
+			if !isPrefix {
+				break
+			}
+			if err != nil {
+				break
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(buffer.String())
+	}
+}
+
+func readString() {
+	fi, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := fi.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	// make a read buffer
+	r := bufio.NewReaderSize(fi, 10)
+
+	var line string
+	for {
+		line, err = r.ReadString('\n')
+		if err != nil {
+			break
+		}
+		fmt.Println(line)
+	}
+	if err != io.EOF {
+		fmt.Println(err)
+	}
+
+}
+
+func scanner() {
+	fi, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := fi.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	scanner := bufio.NewScanner(fi)
+	scanner.Buffer(make([]byte, 2), bufio.MaxScanTokenSize)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	//go_1_compatible()
 	//use_bufio()
-	use_ioutil()
+	//use_ioutil()
+	readLine()
+	//readString()
+	//scanner()
 }
